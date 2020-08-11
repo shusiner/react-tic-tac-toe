aab74269b29f9c880370f4c4dd9ca64d256bbdce
ng huan ran <quantum.plato@gmail.com>
2020-08-11 18:06:57 +0800

    Updates

---
+++ precache-manifest.4d7e39c5ff46adec27d0f97b0cfd39e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35264e17fad4094a1aa7ec72e9a9ba7",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6f23d896e4f47fb6f6eb",
+    "url": "/react-tic-tac-toe/static/css/main.9c567cee.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f23d896e4f47fb6f6eb",
+    "url": "/react-tic-tac-toe/static/js/main.1e188065.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
